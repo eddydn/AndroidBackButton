474e3c7f10f6e2bd3f854f860f8806511d95309e
eddydn <eddydn@gmail.com>
2016-11-21 07:11:31 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidBackButton
+{
+    [Activity(Label = "AndroidBackButton", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+
+        public override void OnBackPressed()
+        {
+            Toast.MakeText(this, "Back Button Pressed Detected", ToastLength.Short).Show();
+
+            Finish();
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
